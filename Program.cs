a0d762c9113bcc14ee147eec124da878e4f726af
Richard Chalk <barmybrit@gmail.com>
2023-07-13 20:13:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HoldsChosenStrategy strategy = new HoldsChosenStrategy();
+
+            strategy.SetStrategy(new Addition());
+            Console.WriteLine("Addition strat: 6+2 = " + strategy.ExecuteStrategy(6, 2));
+
+            strategy.SetStrategy(new Subtraction());
+            Console.WriteLine("Subtraction strat: 6-2 = " + strategy.ExecuteStrategy(6, 2));
+
+            strategy.SetStrategy(new Multiplication());
+            Console.WriteLine("Multiplication strat: 6*2 = " + strategy.ExecuteStrategy(6, 2));
+
+            strategy.SetStrategy(new Division());
+            Console.WriteLine("Division strat: 6/2 = " + strategy.ExecuteStrategy(6, 2));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Strategies/HoldsChosenStrategy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperators
+{
+    class HoldsChosenStrategy
+    {
+        private IStrategy _strategy;
+
+        public void SetStrategy(IStrategy newStrategy)
+        {
+            _strategy = newStrategy;
+        }
+
+        public int ExecuteStrategy(int a, int b)
+        {
+            return _strategy.Execute(a, b);
+        }
+    }
+}
